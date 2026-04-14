557106fc12a06ee6b6ce5a0e8dbfd59d02c6f74c
RKhan2412 <raaid24khan@gmail.com>
2026-04-14 00:15:10 -0400

    Should lead to a signed in page

--- login.js
+++ login.js
@@ -19,5 +19,5 @@ document.getElementById("login-btn").addEventListener("click", async () => {
   }
 
   alert("Login successful!");
-  window.location.href = "index.html";
+  window.location.href = "landinglog.html";
 });
